c3016717649e81f461893b9714b8059d988b5426
MaxKorovchenko <109962117+MaxKorovchenko@users.noreply.github.com>
2022-10-20 10:43:30 +0000

    Deploying to gh-pages from @ MaxKorovchenko/goit-icecream@0994ccb0cf1daab0f92ad2e3b892e1cdc9a18401 🚀

---
+++ index.9004af1a.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-hero-arrow-open]"),closeModalBtn:document.querySelector("[data-modal-hero-arrow-close]"),modal:document.querySelector("[data-modal-hero-arrow]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.9004af1a.js.map
